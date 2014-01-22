4e61ff65d44636e9c9d483d6fa88479d7926ec33
Colby Goettel <colby.goettel@gmail.com>
2014-01-21 18:02:36 -0700

    Added all book directories. Committing before testing.

--- translate-character.pl
+++ translate-character.pl
@@ -6,44 +6,54 @@ use warnings;
 my $DEBUG = 1;
 
 # Open directory.
-my $directory = "Genesis";
-opendir DIR, "$directory" or die "Cannot open DIR: $!\n";
-my @files = sort readdir(DIR);
+my @directories = ["1Kings", "1Samuel", "2Chronicles", "2Kings", "2Samuel", "Daniel", "Deuteronomy", "Exodus", "Ezra", "Isaiah", "Jeremiah", "Joshua", "Judges", "Malachi", "Nehemiah", "Numbers", "Obadiah", "Ruth", "Zephaniah"];
 
-# Run through each file in open directory translating % to %%.
-while ( my $current_file = shift @files )
+foreach @directories
 {
-    next if $current_file =~ /^\./;
-    print "Current file: " . $current_file . "\n" if $DEBUG;
-    
-    open IN, "<$directory/$current_file" or die "Failed to open IN: $!\n";
-    open TMP, ">/tmp/translate-character.tmp" or die "Failed to open TMP: $!\n";
-    
-    while ( <IN> )
-    {
-        chomp(my $current_line = $_);
-        # print "\$_:\t" . $current_line . "\n" if $DEBUG;
-        $current_line =~ s/\%/\%\%/;
-        # print "cl:\t" . $current_line . "\n" if $DEBUG;
-        print TMP $current_line . "\n";
-    }
-    
-    close IN;
-    close TMP;
-    
-    # Read from TMP file, print to original IN file.
-    open TMP_IN, "</tmp/translate-character.tmp" or die "Failed to open TMP: $!\n";
-    open OUT, ">$directory/$current_file" or die "Failed to open OUT: $!\n";
-    
-    while ( <TMP_IN> )
-    {
-        print OUT $_;
-    }
-    
-    close TMP_IN;
-    close OUT;
+    translate-character($_);
 }
 
-closedir DIR;
-close IN;
-close OUT;
+sub translate-character
+{
+    my $directory = shift;
+    opendir DIR, "$directory" or die "Cannot open DIR: $!\n";
+    my @files = sort readdir(DIR);
+
+    # Run through each file in open directory translating % to %%.
+    while ( my $current_file = shift @files )
+    {
+        next if $current_file =~ /^\./;
+        print "Current file: $directory/$current_file\n" if $DEBUG;
+        
+        open IN, "<$directory/$current_file" or die "Failed to open IN: $!\n";
+        open TMP, ">/tmp/translate-character.tmp" or die "Failed to open TMP: $!\n";
+        
+        while ( <IN> )
+        {
+            chomp(my $current_line = $_);
+            # print "\$_:\t" . $current_line . "\n" if $DEBUG;
+            $current_line =~ s/\%/\%\%/;
+            # print "cl:\t" . $current_line . "\n" if $DEBUG;
+            print TMP $current_line . "\n";
+        }
+        
+        close IN;
+        close TMP;
+        
+        # Read from TMP file, print to original IN file.
+        open TMP_IN, "</tmp/translate-character.tmp" or die "Failed to open TMP: $!\n";
+        open OUT, ">$directory/$current_file" or die "Failed to open OUT: $!\n";
+        
+        while ( <TMP_IN> )
+        {
+            print OUT $_;
+        }
+        
+        close TMP_IN;
+        close OUT;
+    }
+
+    closedir DIR;
+    close IN;
+    close OUT;
+}
