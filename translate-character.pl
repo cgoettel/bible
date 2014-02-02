a4c1831240c0b7802156b1e1eb2833a2c765833a
Colby Goettel <colby.goettel@gmail.com>
2014-02-01 19:36:02 -0700

    Removed \chap from all chapters. Put it in heading.

--- translate-character.pl
+++ translate-character.pl
@@ -6,17 +6,17 @@ use warnings;
 my $DEBUG = 1;
 
 # Open directory.
-# my @directories = ("Sandbox");
-my @directories = ("1Kings", "1Samuel", "2Chronicles", "2Kings", "2Samuel", "Daniel", "Deuteronomy", "Exodus", "Ezra", "Genesis", "Isaiah", "Jeremiah", "Joshua", "Judges", "Malachi", "Nehemiah", "Numbers", "Obadiah", "Ruth", "Zephaniah");
+my @directories = ("Sandbox");
+# my @directories = ("1Kings", "1Samuel", "2Chronicles", "2Kings", "2Samuel", "Daniel", "Deuteronomy", "Exodus", "Ezra", "Genesis", "Isaiah", "Jeremiah", "Joshua", "Judges", "Malachi", "Nehemiah", "Numbers", "Obadiah", "Ruth", "Zephaniah");
 
 foreach ( @directories )
 {
     print "Current dir:\t$_\n" if $DEBUG;
     translate($_);
 }
-
 sub translate
 {
+    my $chapter_number = 0;
     my $directory = shift;
     opendir DIR, "$directory" or die "Cannot open DIR: $!\n";
     my @files = sort readdir(DIR);
@@ -33,9 +33,7 @@ sub translate
         while ( <IN> )
         {
             chomp(my $current_line = $_);
-            # print "\$_:\t" . $current_line . "\n" if $DEBUG;
-            $current_line =~ s/\\begin{enumerate\*}/\\begin{enumerate\*}[mode=unboxed]/;
-            # print "cl:\t" . $current_line . "\n" if $DEBUG;
+            $current_line =~ s/\\chap{.*//;
             print TMP $current_line . "\n";
         }
         
