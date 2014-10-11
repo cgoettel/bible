10215b80e72c551c58b6c614558e56805cfbc89f
Colby Goettel <colby.goettel@gmail.com>
2014-10-11 01:30:35 -0600

    3 verses/day.

--- paginate.pl
+++ paginate.pl
@@ -9,7 +9,8 @@ chomp($chapter = <>);
 print "How many verses are there? ";
 chomp($total_verses = <>);
 
-open OUT,">$book/$chapter.tex" or die "Failed to open OUT: $!\n";
+# The append is a fail safe so that if the chapter exists, it's not erased. And yes, I know that you can go into Git and retrieve the old version, but this is easier. And now we don't even have to check for existence. Oh, but Colby, it's only an "if ( -e whatever )". Well whatever.
+open OUT,">>$book/$chapter.tex" or die "Failed to open OUT: $!\n";
 
 print OUT "\\heading{$chapter}{xxxx}\n\n";
 print OUT "\\begin{inparaenum}\n";
@@ -22,3 +23,5 @@ for ( $i = 1; $i <= $total_verses; $i++ )
 print OUT "\\end{inparaenum}\n";
 
 close OUT;
+
+`git add $book/$chapter.tex`;
