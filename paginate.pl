175cb39049a36fbc3e1a545c00b45a405a28274c
Colby Goettel <colby.goettel@gmail.com>
2017-06-26 22:18:04 -0500

    Added Gen 27. Preliminary change to 2 space indent.

--- paginate.pl
+++ paginate.pl
@@ -15,7 +15,7 @@ chomp(my $total_verses = <>);
 
 # The append is a fail safe so that if the chapter exists, it's not erased. And yes, I know that you can go into Git and retrieve the old version, but this is easier. And now we don't even have to check for existence. Oh, but Colby, it's only an "if ( -e whatever )". Well whatever.
 if ( !-d $book ) {
-    make_path $book or die "Failed to create path $book: $!\n";
+  make_path $book or die "Failed to create path $book: $!\n";
 }
 
 open OUT,">>$book/$chapter.tex" or die "Failed to open OUT: $!\n";
@@ -25,7 +25,7 @@ print OUT "\\begin{inparaenum}\n";
 
 for ( my $i = 1; $i <= $total_verses; $i++ )
 {
-    print OUT "    \\verse{$chapter:$i} %%\n";
+  print OUT "  \\verse{$chapter:$i} %%\n";
 }
 
 print OUT "\\end{inparaenum}\n";
